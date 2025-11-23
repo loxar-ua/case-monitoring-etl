3ee6d0a43fcd5040f41f8e388182db09a14d6ff6
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2025-11-23 21:59:09 +0200

    feat: add scrapper for antac
    # Conflicts:
    #	tests/scrapper/scrappers/test_bihus_info_scrapper.py

--- src/scrapper/configs.py
+++ src/scrapper/configs.py
@@ -29,3 +29,31 @@ BIHUS_CFG = {
         "formatter": normalize_text,
     },
 }
+
+ANTAC_CFG = {
+    "title": {
+        "tag_name": "meta",
+        "tag_attrs": {"property": "og:title"},
+        "formatter": str,
+    },
+    "author": {
+        "tag_name": None,
+        "tag_attrs": None,
+        "formatter": str,
+    },
+    "featured_image_url": {
+        "tag_name": "meta",
+        "tag_attrs": {"property": "og:image"},
+        "formatter": str,
+    },
+    "published_at": {
+        "tag_name": "meta",
+        "tag_attrs": {"property": "article:modified_time"},
+        "formatter": datetime.fromisoformat,
+    },
+    "content": {
+        "tag_name": "article",
+        "tag_attrs": {"class": "article-content"},
+        "formatter": normalize_text,
+    },
+}
