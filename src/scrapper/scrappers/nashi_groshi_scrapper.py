ff416fbc18d6fd7e051d6bd72783a755794feac3
Lesia <lesia.liashko.shi.2024@lpnu.ua>
2025-11-26 15:08:44 +0200

    added yoast for groshi,changed tests of mine

--- src/scrapper/scrappers/nashi_groshi_scrapper.py
+++ src/scrapper/scrappers/nashi_groshi_scrapper.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from src.scrapper.scrappers.base_scrapper import BaseScrapper
-from src.scrapper.scrappers.yoast_sitemap import get_links_yoast
+from src.scrapper.scrappers.yoast_nashi import get_links_yoast
 from src.database.models.media import Media
 from src.scrapper.configs import GROSHI_CFG
 
@@ -17,7 +17,8 @@ class NashiGroshiScrapper(BaseScrapper):
 
         links = get_links_yoast(
             sitemap_index_url=self.media_orm.sitemap_index_url,
-            sub_sitemaps_pattern=r"https://nashigroshi\.org/sitemap-pt-post-(202[5-9]|20[3-9]\d|\d{4})-\d{2}\.xml",        start_date=start_date,
+            sub_sitemaps_pattern=r"https://nashigroshi\.org/sitemap-pt-post-(202[5-9]|20[3-9]\d)-\d{2}\.xml",
+            start_date=start_date,
             end_date=end_date,
         )
 
--- src/scrapper/scrappers/yoast_nashi.py
+++ src/scrapper/scrappers/yoast_nashi.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
-from datetime import datetime, timezone
+from datetime import datetime
 import re
 
 from src.utils.get_response import get_response
@@ -17,42 +17,38 @@ def get_links_yoast(sitemap_index_url, sub_sitemaps_pattern, start_date, end_dat
 
     sitemap_index_soup = BeautifulSoup(sitemap_index_response.content, "lxml-xml")
 
-    sub_sitemap_urls = [sitemap.find('loc').text
+    sub_sitemap_urls = [LinkInfo(sitemap.find('loc').text,
+                                 datetime.fromisoformat(sitemap.find('lastmod').text))
                         for sitemap in sitemap_index_soup.find_all("sitemap")]
 
     # Filter all unimportant sitemaps, that don't contain articles
     sub_sitemap_urls = list(filter(
-        lambda x: re.fullmatch(sub_sitemaps_pattern, x),
+        lambda x: re.fullmatch(sub_sitemaps_pattern, x.link),
         sub_sitemap_urls
     ))
 
     combined_sub_sitemap = BeautifulSoup("<urlset></urlset>", "lxml-xml")
     for sub_sitemap_url in sub_sitemap_urls:
 
-        sub_sitemap_response = get_response(sub_sitemap_url)
+        sub_sitemap_response = get_response(sub_sitemap_url.link)
 
         if sub_sitemap_response is None:
             continue
 
         sub_sitemap_soup = BeautifulSoup(sub_sitemap_response.content, "lxml-xml")
-        print("DEBUG XML:", sub_sitemap_soup.prettify()[:500])
 
-        for url_tag in sub_sitemap_soup.urlset.find_all(recursive=False):
-            combined_sub_sitemap.urlset.append(url_tag)
+        urlset_tag = sub_sitemap_soup.find("urlset")
+        if urlset_tag:
+            for url_tag in urlset_tag.find_all(recursive=False):
+                combined_sub_sitemap.urlset.append(url_tag)
 
-    article_urls = [LinkInfo(url.find("loc").text.strip(),
-                             datetime.fromisoformat(url.find("lastmod").text)
-                             .replace(tzinfo=timezone.utc))
+    article_urls = [LinkInfo(url.find("loc").text,
+                             datetime.fromisoformat(url.find("lastmod").text))
                     for url in combined_sub_sitemap.urlset]
 
     article_urls = list(filter(
         lambda x: start_date <= x.datetime <= end_date,
         article_urls
-    ))
-
-    article_urls = list(sorted(
-        article_urls,
-        key=lambda x: x.datetime
     ))
 
     links = [article_url.link for article_url in article_urls]
