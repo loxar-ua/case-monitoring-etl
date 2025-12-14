5cbafb491374be34702db2783d6c32a365c46c27
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2025-12-14 18:46:38 +0200

    update: add dense and sparse embedding columns to article. Return is_relevant column to article model

--- src/database/models/article.py
+++ src/database/models/article.py
@@ -1,12 +1,15 @@
 from sqlalchemy import Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from pgvector.sqlalchemy import Vector
+from pgvector.sqlalchemy import Vector, SparseVector
 
 from datetime import datetime
 
 from .base import Base
 from .media import Media
 from .cluster import Cluster
+
+SPARSE_EMBEDDING_SIZE = 250002 # TODO: after implementing vector creation in demployment, change it to be dynamic
+DENSE_EMBEDDING_SIZE = 1024
 
 class Article(Base):
     __tablename__ = "article"
@@ -17,8 +20,10 @@ class Article(Base):
     author: Mapped[str] = mapped_column(String, nullable=True)
     content: Mapped[str] = mapped_column(String)
     published_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-    embedding: Mapped[list] = mapped_column(Vector(1), default=None, nullable=True) #TODO: change dimension size and list type
+    dense_embedding: Mapped[list] = mapped_column(Vector(DENSE_EMBEDDING_SIZE), default=None, nullable=True)
+    sparse_embedding: Mapped[dict] = mapped_column(SparseVector(SPARSE_EMBEDDING_SIZE), default=None, nullable=True)
     is_checked: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_relevant: Mapped[bool] = mapped_column(Boolean, default=False)
 
     cluster_id: Mapped[int] = mapped_column(ForeignKey("cluster.id"), nullable=True)
     cluster: Mapped["Cluster"] = relationship(back_populates="articles")
