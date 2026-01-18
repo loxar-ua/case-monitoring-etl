bf98518154d291e1de976aab82f140bd7c25c529
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2026-01-18 20:33:41 +0200

    fix: work differently with lists and other parameters of batcher

--- src/utils/batcher.py
+++ src/utils/batcher.py
@@ -4,18 +4,25 @@ from src.logger import logger
 
 def batcher(batch_size: int):
     def decorator(func):
-        def wrapper(size, elements, *args, **kwargs):
+        def wrapper(size, *args, **kwargs):
             chunk_number = ceil(size / batch_size)
 
             for i in range(chunk_number):
                 chunk_start = i * batch_size
                 chunk_end = min((i + 1) * batch_size, size)
-                current_chunk = elements[chunk_start:chunk_end]
-                current_size = len(current_chunk)
 
-                logger.info("Processing batch of size %s", current_size)
-                batch_result = func(current_size, current_chunk, *args, **kwargs)
-                logger.info("Finished processing batch of size %s", current_size)
+                batch_args = []
+                for arg in args:
+                    if isinstance(arg, (list, tuple)) and len(arg) == size:
+                        batch_args.append(arg[chunk_start:chunk_end])
+                    else:
+                        batch_args.append(arg)
+
+                current_real_size = chunk_end - chunk_start
+
+                logger.info("Processing batch of size %s", current_real_size)
+                batch_result = func(current_real_size, *batch_args, **kwargs)
+                logger.info("Finished processing batch of size %s", current_real_size)
 
                 if batch_result:
                     yield from batch_result
